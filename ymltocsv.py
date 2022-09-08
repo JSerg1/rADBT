98c4dca7f975a8fee535821819866615381f1345
Prast <hihihi0102@gmail.com>
2022-09-09 01:28:28 +0900

    [Update] 예외처리 추가

--- ymltocsv.py
+++ ymltocsv.py
@@ -15,6 +15,7 @@
 from tkinter import *
 from tkinter import filedialog
 from ymltocsv_func import yml_to_csv
+import tkinter.messagebox as msgbox
 
 root = Tk()
 root.title("Database yml to csv") # 프로그램 제목타이틀
@@ -24,32 +25,35 @@ root.resizable(False, False) # 프로그램 크리 변경 불가
 # dir path 함수 정의
 def dir_path():
     folder_dir = filedialog.askdirectory()
-    if folder_dir is None: #사용자가 취소를 누를 때
+    if folder_dir == '': #사용자가 취소를 누를 때
         return
     #prunt(folder_selected)
     RE_dir_path.delete(0, END)
     RE_dir_path.insert(0, folder_dir)
-    
-# 타이틀, 제작자, 블로그, 입력설명
-titlename = Label(root, text="rAhtena database - yml to csv (ID, AegisName, Name)", font="arial 16 bold") #제목
-titlename.place(x=73, y=15)
-makers = Label(root, text="제작자 : SerG") #제작자
-makers.place(x=580, y=120)
-blogs = Label(root, text="https://blog.naver.com/hihihi0102") #블로그주소
-blogs.place(x=467, y=140)
-dir_path_desc = Label(root, text="rAthena\\db\\re 폴더 경로 입력") #입력창설명
-dir_path_desc.place(x=14, y=60)
 
-# RE 폴더 경로
-RE_dir_path = Entry(root, width=80)
-RE_dir_path.place(x=15, y=84)
+try:
+    # 타이틀, 제작자, 블로그, 입력설명
+    titlename = Label(root, text="rAhtena database - yml to csv (ID, AegisName, Name)", font="arial 16 bold") #제목
+    titlename.place(x=73, y=15)
+    makers = Label(root, text="제작자 : SerG") #제작자
+    makers.place(x=580, y=120)
+    blogs = Label(root, text="https://blog.naver.com/hihihi0102") #블로그주소
+    blogs.place(x=467, y=140)
+    dir_path_desc = Label(root, text="rAthena\\db\\re 폴더 경로 입력") #입력창설명
+    dir_path_desc.place(x=14, y=60)
 
-# 찾기 버튼
-btn1_dirpath = Button(root, width=8, text="찾기", relief="raised", command=dir_path)
-btn1_dirpath.place(x=590, y=80)
+    # RE 폴더 경로
+    RE_dir_path = Entry(root, width=80)
+    RE_dir_path.place(x=15, y=84)
 
-# 시작버튼
-btn2_start = Button(root, padx=10, pady=1, text="yml to csv", font="arial 10", command=lambda:yml_to_csv(RE_dir_path.get() + "\\"))
-btn2_start.place(x=310, y=120)
+    # 찾기 버튼
+    btn1_dirpath = Button(root, width=8, text="찾기", relief="raised", command=dir_path)
+    btn1_dirpath.place(x=590, y=80)
+
+    # 시작버튼
+    btn2_start = Button(root, padx=10, pady=1, text="yml to csv", font="arial 10", command=lambda:yml_to_csv(RE_dir_path.get() + "\\"))
+    btn2_start.place(x=310, y=120)
+except Exception as err: #예외처리
+    msgbox.showerror("에러", err)
 
 root.mainloop()
