68871743c0a601430ffe3ce26265639c6bafbcec
Prast <hihihi0102@gmail.com>
2022-09-12 10:45:03 +0900

    [Update] 방식 변경 (변경전: 한국어 명칭이 없을경우 항목이 삭제됨 -> 변경후: 한국어명칭이 없어도 항목 유지)

--- csv_insert_func.py
+++ csv_insert_func.py
@@ -3,33 +3,40 @@ import os
 import csv
 
 def insert_korean(txt_path, csv_path):
-    print(txt_path)
-    print(csv_path)
-    dir = os.path.dirname(csv_path)
+    print(txt_path) # ko 아이템 db 경로 확인
+    print(csv_path) # 추출 csv db 경로 확인
+    dir = os.path.dirname(csv_path) 
     filename1 = os.path.basename(csv_path)
-    ko_csv = open(txt_path, 'r', newline='', encoding="cp949")
-    eg_csv = open(csv_path, 'r', newline='', encoding="cp949")
-    new_csv = open(dir + '\\merged_' + filename1, 'w')
-    new_csv.write('ID' + ',' + 'AeigsName' + ',' + 'Name' + ',' + 'Trans_AeigsName' + ',' + 'Trans_Name' + '\n')
-    ko_csv_read = csv.reader(ko_csv)
+    ko_csv = open(txt_path, 'r', newline='', encoding="cp949") # ko 아이템 db 파일 열기
+    eg_csv = open(csv_path, 'r', newline='', encoding="cp949") # 추출 csv db 열기
+    new_csv = open(dir + '\\merged_' + filename1, 'w') # 저장할 csv 파일 이름 및 경로 설정(같은폴더 강제, 앞에  merged_ 붙음)
+    ko_csv_read = list(csv.reader(ko_csv))
     en_csv_read = list(csv.reader(eg_csv))
     
-    for i in ko_csv_read:
-        ko_val = i
-        id1 = ko_val[0]
-        aegis_name1 = ko_val[1]
-        item_name1 = ko_val[2]
-        for j in en_csv_read:
-            eg_val = j
-            id2 = eg_val[0]
-            aegis_name2 = eg_val[1]
-            item_name2 = eg_val[2]
-            if id1 == id2:
-                new_csv.write(str(id1) + ',' + str(aegis_name1) + ',' + str(item_name1) + ',' + str(aegis_name2) + ',' + str(item_name2) + '\n')
-                
+    for i in en_csv_read:
+        temp_val1 = i
+        id1 = temp_val1[0]
+        en_aegis_name = temp_val1[1]
+        en_item_name = temp_val1[2]
+        save_ko_aegis_name = temp_val1[3] # save_ko_aegis_name, save_ko_item_name 은 기본적으로 추출한 csv 파일의 trans 열이므로, 디폴트는 null 값임
+        save_ko_item_name = temp_val1[4]
+        for j in ko_csv_read:
+            temp_var2 = j
+            id2 = temp_var2[0]
+            ko_aegis_name = temp_var2[1]
+            ko_item_name = temp_var2[2]
+            if id1 == id2: # id1 과 id2의 확인작업을 거친 후, id 넘버가 일치하는 항목만 save_ko_aegis_name, save_ko_item_name 항목에 한글 db 명을 넣음.
+                save_ko_aegis_name = ko_aegis_name
+                save_ko_item_name = ko_item_name
+        new_csv.write(str(id1) + ',' + str(en_aegis_name) + ',' + str(en_item_name) + ',' + str(save_ko_aegis_name) + ',' + str(save_ko_item_name) + '\n')
+    
+    ko_csv.close()
+    eg_csv.close()
+    new_csv.close()
+    
     msgbox.showinfo("Complite", "한글 파일과 합치는 작업이 완료되었습니다.")
 
 # This code used only test
-# test_val1 = 'C:/Users/Lee/Desktop/PythonWorkspace/rADBTP/korean_insert/ko_item_db_test1_kr.csv'
-# test_val2 = 'C:/Users/Lee/Desktop/PythonWorkspace/rADBTP/korean_insert/ko_item_db_test2_eg.csv'
+# test_val1 = 'C:/Users/Lee/Desktop/PythonWorkspace/rADBTP/korean_insert/ko_item_db.csv'
+# test_val2 = 'C:/Users/Lee/Desktop/PythonWorkspace/rADBTP/korean_insert/item_db_equip.csv'
 # insert_korean(test_val1, test_val2)
